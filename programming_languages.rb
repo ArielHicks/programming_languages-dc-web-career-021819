e7d4d59d0fa491cb9952b71c3bff98496d00bac7
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-13 21:38:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -37,4 +37,15 @@ def reformat_languages(languages)
 
     languages.each do |oo_or_functional, language_hash|
        #:oo, {:ruby => {...}}
+       language_hash.each do |language, attribute_hash|
+       #:ruby, {:type => "interpreted"}
+       attribute_hash.each do |attribute, str_value|
+         #:type, "interpreted"
+         if language_attriutes[language].nil?
+           #if language :ruby doesn't exist in new hash
+           language_attributes[language] = {} #initiate a new hash for the value of :ruby
+         end
+         # this would cause us problems if we had more than one attribute we're iterating over
+
+
 end
