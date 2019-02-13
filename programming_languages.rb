579c61beeca01e2662c06cfa2ea4fb90eb72e8ea
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-13 21:30:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,40 @@
+#
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
+#
+#   }
+# }
+
 def reformat_languages(languages)
-  # your code here
+    # your code here
+    hash = {}
+    # hash[:my_key] = {second_level_key: "second level value"}
+    hash[:ruby] = {type: "interpreted", style: [:oo]}
+    hash
 end
